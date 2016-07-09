ab365ceaf685be730ce3fd5f753fa0c8f8257555
Jeff Fichtner <jff2501@gmail.com>
2016-07-09 20:35:28 +0200

    Finishes release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+#vampire interview
+
+#data gathering
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+birthyear = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
