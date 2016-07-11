1f53ccc8ad94e9149eb766e5ad60860300bda79b
Jeff Fichtner <jff2501@gmail.com>
2016-07-11 14:20:09 +0200

    Adds question methods, adds partially-working allergy loop, needs employee loop fix

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,67 +1,97 @@
 #vampire interview
 
+#question method
+def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+
+		if reply == "yes"
+			return true
+		elsif reply == "no"
+			return false
+		else
+			puts "Please answer with \"yes\" or \"no\"."
+		end
+	end
+end
+
+#integer question method
+def int question
+	while true
+		puts question
+		reply = gets.chomp.to_i
+
+		if reply > 0
+			break
+		elsif reply <= 0
+			puts "Please answer with an appropriate number."
+		end
+	end
+end
+
 #initiate loop
 puts "How many employees would you like to process?"
 n_employees = gets.chomp.to_i
 n = 0
 while n < n_employees
 
-#data gathering
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-birthyear = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-insurance = gets.chomp
+	#data gathering
+	puts "What is your name?"
+	name = gets.chomp.downcase
+	age = int "How old are you?"
+	birthyear = int "What year were you born?"
+	garlic = ask "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+	insurance = ask "Would you like to enroll in the company’s health insurance? (yes/no)"
 
-#calculate age
-age_year = 2016 - birthyear
+	#allergy loop
+	puts "Please list any allergies you may have. Type \"done\" when you're finished."
+	while true
+		allergy = gets.chomp.downcase
+		if allergy == "sunshine"
+			puts "Probably a vampire."
+			break
+		elsif allergy == "done"
+			break
+		else
+		end
+	end
 
-#boolean conversion methods
-if age_year == age
-	age_is_correct = true
-else
-	age_is_correct = false
-end
-
-if garlic == "yes"
-	likes_garlic = true
-else
-	likes_garlic = false
-end
-
-if insurance == "yes"
-	wants_insurance = true
-else
-	wants_insurance = false
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-	vampire_name = true
-else
-	vampire_name = false
-end
-
-#detection
-puts case
-	when vampire_name
-		"Definitely a vampire."
-	when !age_is_correct && !likes_garlic && !wants_insurance
-		"Almost certainly a vampire."
-	when !age_is_correct && ( !likes_garlic || !wants_insurance )
-		"Probably a vampire."
-	when age_is_correct && ( likes_garlic || wants_insurance )
-		"Probably not a vampire."
+	#converting age to bool
+	age_year = 2016 - birthyear
+	if age_year == age
+		age_is_correct = true
 	else
-		"Results inconclusive."	
-end
+		age_is_correct = false
+	end
 
-#end loop
-n += 1
+	#converting name to bool
+	if name == "drake cula" || name == "tu fang"
+		vampire_name = true
+	else
+		vampire_name = false
+	end
+
+	#detection
+	if allergy == "sunshine"
+		break
+	else
+		puts case
+			when vampire_name
+				"Definitely a vampire."
+			when !age_is_correct && !likes_garlic && !wants_insurance
+				"Almost certainly a vampire."
+			when !age_is_correct && ( !likes_garlic || !wants_insurance )
+				"Probably a vampire."
+			when age_is_correct && ( likes_garlic || wants_insurance )
+				"Probably not a vampire."
+			else
+				"Results inconclusive."	
+		end
+	end
+
+	#end loop
+	n += 1
 end
 
 #just kidding
