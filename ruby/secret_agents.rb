c98c3954ef8cbb7ce4212471834f0c335aab7125
Jeff Fichtner <jff2501@gmail.com>
2016-07-13 01:38:41 +0200

    Fixes returns of methods, cleans up pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,68 +1,80 @@
 #secret agent encryption
 
-=begin encryption (pseudocode)
+=begin
+
+encryption (pseudocode)
 
 -define the method (argument)
 	-define index variable
-	-count the string
+	-define empty return string variable
 	-create a loop that cycles through each index until remaining character
 		-if it's a letter (if it's NOT a space)
-		    -advance one letter forward
-		    -print it
-		-elsif "z"
-		    -print "a"
+			-advance one letter forward
+			-add to return string
+		-if "z"
+	    	-add "a" to return string
 		-end
 	-end loop
+	-return the full return string
 -end method
 
 =end
 
 def encrypt str
   index = 0
+  return_str = ""
   until index >= str.length
     if str[index] != " " && str[index] != "z"
       x = str[index].next
-      print x
+      return_str = return_str + x
     elsif str[index] == "z"
       x = "a"
-      print x
+      return_str = return_str + x
     else
       x = str[index]
-      print x
+      return_str = return_str + x
     end
     index += 1
   end
+  return return_str
 end
 
 
 
-=begin decryption (pseudocode)
+=begin
+
+decryption (pseudocode)
 
 -define the method (argument)
 	-define index variable
+	-define empty return string
 	-create alphabet string
-	-count the string
 	-create a loop that cycles through each index until remaining character
-		if its not a space
-		    -find the matching index
-		    -subtract the index by one
-		    -rematch it with alphabet string
-		    -print the result
-		end
-		add one to the index
-	end the loop
-end the method
+		-if it's not a space
+			-find the matching index
+			-subtract the index by one
+			-rematch it with alphabet string
+			-add to return string
+		-end
+		-add one to the index
+	-end the loop
+	-return the full return string
+-end the method
 
 =end
 
 def decrypt str
   n = 0
+  return_str = ""
   alpha = "abcdefghijklmnopqrstuvwxyz"
   until n >= str.length
     if str[n] != " "
       x = alpha.index(str[n]) - 1
-      print alpha[x]
+      return_str = return_str + alpha[x]
     end
     n += 1
   end
+  return return_str
 end
+
+puts decrypt(encrypt("swordfish"))
