167c0ba1f497949a2eb144f87af0d235e49ddf03
Jeff Fichtner <jff2501@gmail.com>
2016-07-12 23:49:34 +0200

    Adds working encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,29 +5,26 @@
 -define the method (argument)
 -count the string
 -create a loop that cycles through each index until remaining character
-	-if it's a letter (if it's NOT a space)
-		-advance one letter forward
-	-end
-	-add to new string
+  -if it's a letter (if it's NOT a space)
+    -advance one letter forward
+  -end
+  -add to new string
 -end loop
 
 =end
 
 def encrypt str
-	index = 0
-	#new_str = ""
-	until index >= str.length
-		if str[index] != " "
-			x = str[index].next
-			puts x
-		else
-			x = str[index]
-			puts x
-		end
-		#new_str = new_str + x
-		index += 1
-	end
-	#new_str
+  index = 0
+  until index >= str.length
+    if str[index] != " "
+      x = str[index].next
+      print x
+    else
+      x = str[index]
+      print x
+    end
+    index += 1
+  end
 end
 
-encrypt("b c")
+encrypt("abc")
