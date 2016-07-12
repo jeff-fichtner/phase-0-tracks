0fb6ab6ed834a9c8c90f8f92702af0558ed28e89
Jeff Fichtner <jff2501@gmail.com>
2016-07-13 01:03:36 +0200

    Adds decryption method and cleans up all pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,13 +3,17 @@
 =begin encryption (pseudocode)
 
 -define the method (argument)
--count the string
--create a loop that cycles through each index until remaining character
-  -if it's a letter (if it's NOT a space)
-    -advance one letter forward
-  -end
-  -add to new string
--end loop
+	-define index variable
+	-count the string
+	-create a loop that cycles through each index until remaining character
+		-if it's a letter (if it's NOT a space)
+		    -advance one letter forward
+		    -print it
+		-elsif "z"
+		    -print "a"
+		-end
+	-end loop
+-end method
 
 =end
 
@@ -30,6 +34,35 @@ def encrypt str
   end
 end
 
-x = encrypt("zed")
 
-puts x
+
+=begin decryption (pseudocode)
+
+-define the method (argument)
+	-define index variable
+	-create alphabet string
+	-count the string
+	-create a loop that cycles through each index until remaining character
+		if its not a space
+		    -find the matching index
+		    -subtract the index by one
+		    -rematch it with alphabet string
+		    -print the result
+		end
+		add one to the index
+	end the loop
+end the method
+
+=end
+
+def decrypt str
+  n = 0
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  until n >= str.length
+    if str[n] != " "
+      x = alpha.index(str[n]) - 1
+      print alpha[x]
+    end
+    n += 1
+  end
+end
