d966d808281bb212865b218f1b3a46914ba38296
Jeff Fichtner <jff2501@gmail.com>
2016-07-20 18:07:29 +0200

    Adds release 0

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,18 @@
+#release 0
+
+def search (arr, int)
+  found = nil
+  i = 0
+  while i < arr.length
+    if arr[i] == int
+      found = i
+    end
+  i += 1
+  end
+  found
+end
+
+#release 1
+
+#release 2
+
