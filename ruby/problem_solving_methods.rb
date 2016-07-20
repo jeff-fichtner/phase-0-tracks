f459f0280dd3aab0d6c767f2205906f451597305
Jeff Fichtner <jff2501@gmail.com>
2016-07-20 19:06:55 +0200

    Adds release 1 and 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,5 +14,38 @@ end
 
 #release 1
 
+def fib(int)
+  fibs = [0]
+  if int == 1
+    return fibs
+  elsif int == 2 
+    fibs << 1 
+    return fibs
+  elsif int == 3
+    fibs << 1 << 1
+    return fibs
+  else 
+    fibs << 1 << 1
+    while int > fibs.length 
+        fibs << fibs[-1] + fibs[-2] 
+    end
+  end
+  fibs 
+end
+
 #release 2
 
+def insertion(arr)
+  i = 1
+  while i < arr.length
+    j = i
+    while j > 0 
+      if arr[j-1] > arr[j] 
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+      end
+      j = j-1
+    end
+    i += 1
+  end
+  arr
+end
