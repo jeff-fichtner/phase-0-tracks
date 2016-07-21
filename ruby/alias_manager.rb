6c499b7e83c331921a8f8c3b4ff8c565a9c76c59
Jeff Fichtner <jff2501@gmail.com>
2016-07-21 15:03:34 +0200

    Adds pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,26 @@
 # alias program
 
 =begin
+
 Pseudocode:
 
+ask for name (downcase)
+split into two object array
+switch items
+for each item:
+split into characters
+if vowel
+	aeiou
+	change to next index in string
+	edge case
+elsif consonant
+	bcdfghjklmnpqrestvwxyz
+	change to next index in string
+	edge case
+rejoin
+capitalize
+store in data structure
+print data
 
 =end
 
