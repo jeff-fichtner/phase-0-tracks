e79081a56310ce83fcea09bf9ae145a953d13c82
Jeff Fichtner <jff2501@gmail.com>
2016-07-25 16:29:03 -0700

    Release 1 adds pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,41 @@
+# Pseudocode
+
+# make an empty hash
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # take the input and split into array
+  # take array, turn them into keys, default quantity 1
+  
+  # print the list to the console (puts) (reference print method)
+# output: hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+  # make loop to verify duplicate
+  # take input and convert to key, default quantity
+# output: updated hash
+
+# Method to remove an item from the list
+# input: name of item to be removed
+# steps: 
+  # print the keys
+  # loop until proper input
+  # delete from the hash (.delete)
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: key and value
+# steps: 
+  # print the keys
+  # loop until proper input
+  # describe new quantity
+  # update the hash
+# output: updated hash
+
+# Method to print a list and make it look pretty
+# input: the hash
+# steps: (puts)
+# output: printed hash
