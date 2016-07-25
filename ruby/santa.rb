402b75f0a389992be541baa39e5c41bcdc655cda
Jeff Fichtner <jff2501@gmail.com>
2016-07-25 09:33:25 -0700

    Adds instance variables (release 1)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,19 +1,35 @@
 class Santa
+	def initialize gender, ethnicity
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+		p "Initializing Santa instance ..."
+	end
 
-def initialize
-	p "Initializing Santa instance ..."
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies cookie_type
+		p "That was a good #{cookie_type} cookie!"
+	end
 end
 
-def speak
-	p "Ho, ho, ho! Haaaappy holidays!"
-end
-
-def eat_milk_and_cookies cookie_type
-	p "That was a good #{cookie_type} cookie!"
-end
-
-end
-
+=begin driver code
 st_nick = Santa.new
 st_nick.speak
 st_nick.eat_milk_and_cookies('chocolate chip')
+=end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas << Santa.new("male", "Colombian")
+santas << Santa.new("female", "alien")
+
+p santas
