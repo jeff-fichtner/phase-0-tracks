a4a707fbee93ddd74d3a357e583b929daf7feeb8
Jeff Fichtner <jff2501@gmail.com>
2016-07-25 10:17:46 -0700

    Adds getter setter methods, instance writing methods, updates driver code (release 2)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,13 +14,41 @@ class Santa
 	def eat_milk_and_cookies cookie_type
 		p "That was a good #{cookie_type} cookie!"
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at reindeer
+		i = @reindeer_ranking.index(reindeer)
+		@reindeer_ranking.delete_at(i)
+		@reindeer_ranking << reindeer
+	end
+
+	def gender= new_gender
+		@gender = new_gender
+	end
+
+	def gender
+		@gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
 end
 
-=begin driver code
-st_nick = Santa.new
+st_nick = Santa.new("intersex", "mixed")
 st_nick.speak
 st_nick.eat_milk_and_cookies('chocolate chip')
-=end
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
@@ -32,4 +60,19 @@ end
 santas << Santa.new("male", "Colombian")
 santas << Santa.new("female", "alien")
 
-p santas
+
+p santas[1].class
+
+p santas[2].ethnicity
+
+p santas[5].age
+santas[5].celebrate_birthday
+p santas[5].age
+
+p santas[3].reindeer_ranking
+santas[3].get_mad_at "Vixen"
+p santas[3].reindeer_ranking
+
+p santas[1].gender
+santas[1].gender = "not sure"
+p santas[1].gender
