9749bc94ff669faf4938e2ddf9838a2da3cc568d
Jeff Fichtner <jff2501@gmail.com>
2016-07-25 06:23:34 -0700

    Adds Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin puppy class
+SPECIES ------------------------
+Heartus Meltus
+
+CHARACTERISTICS ----------------
+Eyelash length: varies
+Man's best friend: true
+Eye count: 2
+Name: varies
+Color: varies
+Collared: true
+
+BEHAVIOR -----------------------
+Eat
+Sleep
+Play
+Snuggle
+Fetch
+Sniff butts
+=end
