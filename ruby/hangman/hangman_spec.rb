0e4c472f6bf3999586c54a7120b349024fbb6c45
Jeff Fichtner <jff2501@gmail.com>
2016-07-29 17:28:03 -0700

    Rewrites test logic, reformats

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -3,39 +3,48 @@ require_relative 'hangman'
 describe Hangman do
   let(:hangman) { Hangman.new("hangman") }
 
-  it "updates game_array" do
-    expect(hangman.update_game_array).to eq nil
+  before do
+    @guess = "n"
+    @guess_array = ["n"]
+  end
+
+  it "updates @game_array" do
+    expect(hangman.update_game_array).to eq ['_','_','n','_','_','_','n']
   end
 
   it "duplicates letter" do
-    expect(hangman.duplicate_letter).to eq ["_", "_", "_", "_", "_", "_", "_"]
-  end
-
-  it "verifies if player won" do
-    expect(hangman.did_player_win).to eq nil
+    expect(hangman.duplicate_letter).to eq ['_','_','n','_','_','_','n']
   end
 
   it "verifies if a guess was repeated" do
-    expect(hangman.was_guess_repeat).to eq nil
+    expect(hangman.was_guess_repeat).to eq true
   end
 
   it "adds current guess to list of all guesses" do
-    expect(hangman.update_guess_array).to eq [""]
-  end
-
-  it "adds to counter if guess was incorrect" do
-    expect(hangman.add_guess_count).to eq 1
-  end
-
-  it "verifies if there are still turns left" do
-    expect(hangman.is_game_over).to eq nil
-  end
-
-  it "displays the remaining turns" do
-    expect(hangman.display_remaining_turns).to eq nil
-  end
-
-  it "prints the end result" do
-    expect(hangman.end_of_game).to eq nil
+    expect(hangman.update_guess_array).to eq ["n","n"]
   end
 end
+
+# describe Hangman do
+#   let(:hangman) { Hangman.new("hangman") }
+  
+#   it "verifies if player won" do
+#     expect(hangman.did_player_win).to eq nil
+#   end
+
+#   it "adds to counter if guess was incorrect" do
+#     expect(hangman.add_guess_count).to eq 1
+#   end
+
+#   it "verifies if there are still turns left" do
+#     expect(hangman.is_game_over).to eq nil
+#   end
+
+#   it "displays the remaining turns" do
+#     expect(hangman.display_remaining_turns).to eq nil
+#   end
+
+#   it "prints the end result" do
+#     expect(hangman.end_of_game).to eq nil
+#   end
+# end
