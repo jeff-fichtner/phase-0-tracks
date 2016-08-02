2a5383d2168f58fae61d53081bccdee471bdcef8
Jeff Fichtner <jff2501@gmail.com>
2016-08-01 21:25:02 -0700

    Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,16 @@ var nameArray = ['Ed','Jackson','Mimi','Euridice'];
 
 colorArray.push('pink');
 nameArray.push('Michael');
+
+var horseObject = {}
+for (var i = 0; i < nameArray.length; i++) {
+	horseObject[nameArray[i]] = colorArray[i];
+}
+
+console.log(horseObject)
+
+// make a loop that goes as long as the length of one of the arrays
+//   for every count:
+//     set key = name array at (count) index, 
+//     value = color array at (count) index
+// end loop
