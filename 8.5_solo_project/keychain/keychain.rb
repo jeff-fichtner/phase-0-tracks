e692148fa0328cb13a6cc123c5e50f2762002c82
Jeff Fichtner <jff2501@gmail.com>
2016-08-13 11:09:31 -0700

    Pseudocode

--- 8.5_solo_project/keychain/keychain.rb
+++ 8.5_solo_project/keychain/keychain.rb
@@ -1,9 +1,7 @@
 # password generator and database
 
 
-# multi-user?
-
-# verify correct user
+# verify user (password-protected access?)
 # display all passwords
 # generate password
 # store password with username/other token
