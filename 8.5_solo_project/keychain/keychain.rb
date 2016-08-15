08dadca7e115a87daaa15b3928a80e15485d6752
Jeff Fichtner <jff2501@gmail.com>
2016-08-15 07:16:38 -0700

    Clean/change pseudo, global database

--- 8.5_solo_project/keychain/keychain.rb
+++ 8.5_solo_project/keychain/keychain.rb
@@ -1,7 +1,7 @@
 # password generator and database
 
 
-# verify user (password-protected access?)
+# verify user
 # display all passwords
 # generate password
 # store password with username/other token
@@ -12,28 +12,38 @@
 require 'sqlite3'
 require 'date'
 
-def new_database
-	db = SQLite3::Database.new("passwords.db")
-	db.results_as_hash = true
-	db
-end
+class Keychain
 
-def new_table indentifier
-	create_table_cmd = <<-SQL
-	  CREATE TABLE IF NOT EXISTS ('passwords_' + indentifier) (
-	    id INTEGER PRIMARY KEY,
-	    website VARCHAR(255),
-	    password VARCHAR(255),
-	    init_date DATE
-	  	)
-	SQL
-end
 
-def create_key db, website, password, init_date
-  db.execute("INSERT INTO <database> (website, password, init_date) VALUES (?, ?, ?)", [website, password, init_date])
-end
+	# create database
+	def initialize
+		$PASSWORDS = SQLite3::Database.new("passwords.db")
+		$PASSWORDS.results_as_hash = true
+		$PASSWORDS
+	end
 
-# password generator
+
+	# create table
+	def new_table indentifier
+		create_table_cmd = <<-SQL
+		  CREATE TABLE IF NOT EXISTS passwords (
+		    id INTEGER PRIMARY KEY,
+		    website VARCHAR(255),
+		    password VARCHAR(255),
+		    init_date DATE
+		  	)
+		SQL
+		$PASSWORDS.execute(create_table_cmd)
+	end
+
+
+	# create key
+	def create_key website, password, init_date
+	  $PASSWORDS.execute("INSERT INTO passwords (website, password, init_date) VALUES (?, ?, ?)", [website, password, init_date])
+	end
+
+
+	# password generator
 	def generate length=14
 		characters = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
 		password = String.new
@@ -47,41 +57,60 @@ end
 		password
 	end
 
-# create table
-# view one entry
-# view every entry
 
-# verify date of passwords
-# alert old passwords
+	# view one entry
+	def view_one_entry id
+		hash = $PASSWORDS.execute("SELECT * FROM passwords WHERE id=?", [id])
+		# display hash better
+	end
+
+
+	# view every entry
+	def view_all
+		hash = $PASSWORDS.execute("SELECT * FROM passwords")
+		# display hash better
+	end
+
+
+	# verify date of passwords
+	def verify_date
+		id_array = Array.new
+		# iterate through each row
+			# gather the date, add 90 days
+
+				# if on or before today's date
+					# add id to array
+				# end if
+		# end iteration
+
+		# return array of id's
+	end
+
+end
 
 
 
 # driver code
 
-p generate(9)
+keychain = Keychain.new
+keychain.new_table
+if keychain.verify_date != nil
+	# for each id:
+			# prompt a new password
+			# overwrite old password
 
 
-
-
-
+# create database
+# create table
+# run 3-month check
+		# if 'y', prompt new password
+		# if 'n', stay silent
 
 # begin driver loop
-
-# 1) new user
-	# create new table
-	# redo
-
-# 2) select user
-	# access pre-existing table
-	# run 3-month check
-		# if 'y', prompt new password
-	
 	# 1- view, 2- add, 3- quit
 		
 		# 1- view
-			# display all websites/primary id's
-			# input = id or 'all'
-			# display
+			# display all id's/websites/passwords
 
 		# 2- add
 			# prompt website
@@ -89,7 +118,8 @@ p generate(9)
 				# yes - generate password
 				# no - prompt for password
 
-			# store
+			# store with date
+			# display individual entry
 
 		# 3- quit
 			# break
