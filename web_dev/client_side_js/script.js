875d95f28907a4c0df052d5025a5885517118773
Jeff Fichtner <jff2501@gmail.com>
2016-08-18 20:28:27 -0700

    Release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,11 +1,22 @@
 console.log("hello world")
 
-var h1Text = document.createTextNode("Tables:");
-var body = document.querySelector('body');
-// body.appendChild(h1Text);
-var parent = body.parentNode;
-parent.insertBefore(h1Text, body);
+var secondTable = document.getElementById('2')
+
+secondTable.addEventListener("mouseover", function() {
+	var h1Text = document.createTextNode("Tables:");
+	var body = document.querySelector('body');
+	// body.appendChild(h1Text);
+	var parent = body.parentNode;
+	parent.insertBefore(h1Text, body);
+}, false);
 
 
 var table = document.querySelector('table');
-table.style.color = "magenta";
+// table.style.color = "magenta";
+
+table.onclick = function() {
+	event.target.style.color = "magenta";
+	setTimeout(function() {
+      table.style.color = "";
+    }, 2000);
+};
