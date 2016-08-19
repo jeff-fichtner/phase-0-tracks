b6d657ce400bbc1af0a5d4c335e0229f511178f2
Jeff Fichtner <jff2501@gmail.com>
2016-08-18 19:56:32 -0700

    Release 1

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,11 @@
 console.log("hello world")
+
+var h1Text = document.createTextNode("Tables:");
+var body = document.querySelector('body');
+// body.appendChild(h1Text);
+var parent = body.parentNode;
+parent.insertBefore(h1Text, body);
+
+
+var table = document.querySelector('table');
+table.style.color = "magenta";
